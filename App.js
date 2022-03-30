952b122614d4a01fe9a6420fcff3a7b440c3f0c4
Alexandre Costa <80131663+AlexandreCosta02@users.noreply.github.com>
2022-03-30 20:27:44 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,109 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, Image, TextInput, Button, TouchableOpacity } from 'react-native';
+import Constants from 'expo-constants';
+
+
+// or any pure javascript modules available in npm
+import { Card } from 'react-native-paper';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { altura: 0, massa: 0, resultado: 0, resultadoText: ''};
+    this.calcular = this.calcular.bind(this);
+  }
+  calcular() {
+    let imc = this.state.massa / (this.state.altura * this.state.altura);
+    let s = this.state;
+    s.resultado = imc;
+    if (s.resultado < 18.5) {
+      s.resultadoText = 'Abaixo do Peso';
+    } else if (s.resultado < 25) {
+      s.resultadoText = 'Peso Normal';
+    } else if (s.resultado < 30) {
+      s.resultadoText = 'Sobrepeso';
+    } else if (s.resultado < 35) {
+      s.resultadoText = 'Obesidade Grau I';
+    } else if (s.resultado < 40) {
+      s.resultadoText = 'Obesidade Grau II';
+    } else {
+      s.resultadoText = 'Obesidade Grau III';
+    }
+    this.setState(s);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.paragraph}>Calculadora IMC</Text>
+        <TextInput style={styles.caixatxt} placeholder="Altura" onChangeText={(altura)=>{this.setState({altura})}}/>
+          <TextInput style={styles.caixatxt} placeholder="Peso" onChangeText={(massa)=>{this.setState({massa})}}/>
+          <View style={styles.botao}>
+            <TouchableOpacity style={styles.button} onPress={this.calcular}>
+              <Text style={styles.buttontext}>Calcular</Text>
+            </TouchableOpacity>
+          </View>
+        <Card style={styles.caixa2}>
+          <Text style={styles.resultado}>{this.state.resultado.toFixed(2)}</Text>
+          <Text style={[styles.resultado,{fontSize:20}]}>{this.state.resultadoText}</Text> 
+        </Card> 
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: 'darkblue',
+    padding: 6,
+  },
+  paragraph: {
+    textTransform: 'uppercase',
+    color: '#d8f3dc',
+    fontSize: 35,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    paddingBottom: 15,
+  },
+
+  caixatxt: {
+    height: 40,
+    margin: 15,
+    borderWidth: 1,
+    padding: 10,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+  },
+  caixa2: {
+    borderRadius: 50,
+    borderColor: 'white',
+    backgroundColor: '#87CEFA',
+  },
+ button:{
+    backgroundColor:"#4682B4",
+    width: 150,
+    height: 50,
+    alignSelf:"center",
+    borderRadius: 50,
+    marginBottom: 30
+  },
+  buttontext:{
+    textTransform:"uppercase",
+    textAlign:"center",
+    padding:9,
+    fontSize:20,
+    fontWeight:'bold',
+    color:"navy",
+  },
+  resultado:{
+    alignSelf:"center",
+    color:"white",
+    fontSize:45,
+    fontWeight:'bold',
+    padding: 6,
+  },
+
+});
